9939fec76a5a17cd79bfeb326ea4eb633d0bef28
alexandrahinojosa543-crear <alexandrahinojosa543@gmail.com>
2026-04-16 11:39:42 -0600

    Archivos de objeto, tabla y formularios creados

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function ejemplo(){
+    const inp_nombre = document.getElementById("name")
+    const nombre = inp_nombre.value
+    alert("Se hizo click en el botón, Nombre:" + nombre);
+}
+
+function BuscarCliente(nombre){
+    console.log(nombre)
+}
